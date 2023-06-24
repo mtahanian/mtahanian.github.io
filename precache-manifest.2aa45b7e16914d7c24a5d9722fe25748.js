b4cd89d81041cabed6dd18f6b8f070d3629e84a5
Milad Tahanian <m.tahanian@outlook.com>
2023-06-23 17:35:01 -0700

    init commit

---
+++ precache-manifest.2aa45b7e16914d7c24a5d9722fe25748.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "611b171ac3cc96915336ff6275cc02a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f92f2d3872f87589b625",
+    "url": "/static/css/2.3eb2a443.chunk.css"
+  },
+  {
+    "revision": "0d595e409f3902992f45",
+    "url": "/static/css/main.e8901382.chunk.css"
+  },
+  {
+    "revision": "f92f2d3872f87589b625",
+    "url": "/static/js/2.b96a0a08.chunk.js"
+  },
+  {
+    "revision": "334a3166bc9976dda67650e8c6f88652",
+    "url": "/static/js/2.b96a0a08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d595e409f3902992f45",
+    "url": "/static/js/main.f87430c9.chunk.js"
+  },
+  {
+    "revision": "5384a9ddfc3aa4afcc12",
+    "url": "/static/js/runtime-main.1d13d3d4.js"
+  }
+]);
